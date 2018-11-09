422e2d70d5186842aa66e0a19dee7b746a760f5d
radhika <radhika@gits.co.id>
2018-11-09 22:16:22 +0700

    Initial Commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Radhika Yusuf on 09/11/18.
+//  Copyright © 2018 Radhika Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
